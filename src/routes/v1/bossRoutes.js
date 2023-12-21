1f776fae5b22985b3502d3c54f079e4aa3bdff12
phamtu14 <20020493@vnu.edu.vn>
2023-12-21 09:26:07 +0700

    boss

--- src/routes/v1/bossRoutes.js
+++ src/routes/v1/bossRoutes.js
@@ -7,14 +7,19 @@ const Router = express.Router()
 
 Router.use( jwtMiddleware.verifyToken )
 
+// lấy ra tất cả quản lý
 Router.get('/manage', roleMiddleware('boss'), bossController.getAllEmployees)
 
+// xóa quản lý nào đó
 Router.delete('/manage/:id', roleMiddleware('boss'), bossController.deleteEmployee)
 
+// tạo thêm 1 điểm giao dịch hoặc tập kết
 Router.post('/place', roleMiddleware('boss'), bossController.createPlace)
 
+// lấy ra tất cả điểm giao dịch hoặc tập kết
 Router.get('/place', roleMiddleware('boss'), bossController.getAllPlaces)
 
+// thống kê
 Router.get('/statistical', bossController.statistical)
 
 export const bossRoutes = Router
